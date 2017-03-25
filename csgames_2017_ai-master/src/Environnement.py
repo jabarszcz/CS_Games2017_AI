42faafb123a9f749f5df39f6f0ab80ae89d73ff3
Samuel <samuel_lavoie@hotmail.com>
2017-03-24 21:50:41 -0400

    unvisit

--- csgames_2017_ai-master/src/Environnement.py
+++ csgames_2017_ai-master/src/Environnement.py
@@ -2,15 +2,15 @@ from hockey.action import Action
 
 class Environnement(object):
     def __init__(self):
-        self.visited = {(i,j): False for j in range(11) for i in range(11)}
+        self.visited = {(i,j): 0 for j in range(11) for i in range(11)}
         self.neighbours = {(i,j): [] for j in range(11) for i in range(11)}
         self.current_pos = (5,5)
 
     def is_visited(self, node):
-        return self.visited[node]
+        return self.visited[node] > 0
 
     def init_pos(self, pos):
-        self.visited[pos] = True
+        self.visited[pos] = 1
         self.current_pos = pos
 
     def get_neighbours(self, node):
@@ -19,14 +19,26 @@ class Environnement(object):
     def visit(self, action):
         delta = Action.move[action]
         new_pos = (self.current_pos[0] + delta[0], self.current_pos[1] + delta[1])
-        self.visited[new_pos] = True
+        self.visited[new_pos] += 1
         self.neighbours[new_pos].append(self.current_pos)
         self.neighbours[self.current_pos].append(new_pos)
         self.current_pos = new_pos
-        print self.current_pos
+
+    def unvisit(self, action):
+        delta = Action.move[action]
+        new_pos = (self.current_pos[0] - delta[0], self.current_pos[1] - delta[1])
+        self.visited[self.current_pos] -= 1
+        self.neighbours[new_pos].remove(self.current_pos)
+        self.neighbours[self.current_pos].remove(new_pos)
+        self.current_pos = new_pos
 
 
 if __name__ == '__main__':
     env = Environnement()
-    print env.is_visited((3,3))
-    print env.get_neighbours((3,3))
+    env.init_pos((4,2))
+    env.visit('north')
+    print env.is_visited((4,1))
+    print env.get_neighbours((4,2))
+    env.unvisit('north')
+    print env.is_visited((4,1))
+    print env.get_neighbours((4,1))
