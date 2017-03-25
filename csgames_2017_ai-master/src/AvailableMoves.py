f9e00755b6b0f96c0e50cc3069b866001e3a470f
Pierrick Arsenault <pierrick.arsenault@polymtl.ca>
2017-03-24 21:59:54 -0400

    test this plz thx

--- csgames_2017_ai-master/src/AvailableMoves.py
+++ csgames_2017_ai-master/src/AvailableMoves.py
@@ -10,43 +10,63 @@ class Direction(Enum):
      DOWNRIGHT = 8
 
 class MovesChecker:
-	def __init__(self, environment):
-		self.environment = environment
-		self.visited = set()
-		self.toVisit = []
-		
-	def availableMoves(self, position):
-		#position[0]:x
-		#position[1]:y
-		self.toVisit.add(position)
-		
-		#Iterate on toVisit positions
-		
-		while(self.toVisit.size):
-			actual = self.toVisit.pop()
-			self.visited.add(actual)
-			tryAllMoves(actual, [])
+    def __init__(self, environment):
+        self.environment = environment
+        self.visited = set()
+        
+    def availableMoves(self, position):
+        #position[0]:x
+        #position[1]:y
+        
+        #Iterate on toVisit positions
+        self.visited.add(position)
+        self.tryAllMoves(position, [])
 
-	def tryAllMoves(self, position, directions):	
-		#todo check if arc is already taken
-		tryMove( (position[0],  position[1]+1), previousDirections + [Direction.UP] )
-		tryMove( (position[0],  position[1]-1), previousDirections + [Direction.DOWN] )
-		tryMove( (position[0]-1,position[1])  , previousDirections + [Direction.LEFT] )
-		tryMove( (position[0]+1,position[1])  , previousDirections + [Direction.RIGHT] )
-		tryMove( (position[0]-1,position[1]+1), previousDirections + [Direction.UPLEFT] )
-		tryMove( (position[0]+1,position[1]+1), previousDirections + [Direction.UPRIGHT] )
-		tryMove( (position[0]-1,position[1]-1), previousDirections + [Direction.DOWNLEFT] )
-		tryMove( (position[0]+1,position[1]-1), previousDirections + [Direction.DOWNRIGHT] )
+    def tryAllMoves(self, position, directions):    
+        #todo check if arc is already taken
+        
+        neigh = set(self.environment.get_neighbours(position))
+        all = set([     
+            (position[0],  position[1]+1),
+            (position[0],  position[1]-1),
+            (position[0]-1,position[1])  ,
+            (position[0]+1,position[1])  ,
+            (position[0]-1,position[1]+1),
+            (position[0]+1,position[1]+1),
+            (position[0]-1,position[1]-1),
+            (position[0]+1,position[1]-1) ])
+        
+        avail = all - neigh
+        
+        if (position[0],  position[1]+1) in avail :
+            self.tryMove( (position[0],  position[1]+1), directions + [Direction.UP] )
+        if (position[0],  position[1]-1) in avail :
+            self.tryMove( (position[0],  position[1]-1), directions + [Direction.DOWN] )
+        if (position[0]-1,position[1]) in avail :
+            self.tryMove( (position[0]-1,position[1])  , directions + [Direction.LEFT] )
+        if (position[0]+1,position[1]) in avail :
+            self.tryMove( (position[0]+1,position[1])  , directions + [Direction.RIGHT] )
+        if (position[0]-1,position[1]+1) in avail :
+            self.tryMove( (position[0]-1,position[1]+1), directions + [Direction.UPLEFT] )
+        if (position[0]+1,position[1]+1) in avail :
+            self.tryMove( (position[0]+1,position[1]+1), directions + [Direction.UPRIGHT] )
+        if (position[0]-1,position[1]-1) in avail :
+            self.tryMove( (position[0]-1,position[1]-1), directions + [Direction.DOWNLEFT] )
+        if (position[0]+1,position[1]-1) in avail :
+            self.tryMove( (position[0]+1,position[1]-1), directions + [Direction.DOWNRIGHT] )
 
-	def tryMove(self, position, directions) #directions so far
-		if(position[0] < 0 or position[0] > 10 or position[1] < 0 or position[1] > 10): 
-			return
-		if(position in self.visited):
-			return
-		if(environment.is_visited(position))
-			tryAllMoves()
-		else
-			print(directions)
-		
-		
-	
+    def tryMove(self, position, directions): #directions so far
+        if(position[0] < 0 or position[0] > 10 or position[1] < 0 or position[1] > 10): 
+            return
+        if(position in self.visited):
+            return
+            
+        self.visited.add(position)
+        
+        if(self.environment.is_visited(position)):
+            self.tryAllMoves(position, directions)
+        else:
+            print(directions)
+        
+        
+    
